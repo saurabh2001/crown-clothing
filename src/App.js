b606b3715f17a68e7b01bfa9481aeb6311900934
saurabh <saury.shah@gmail.com>
2019-09-22 00:02:58 +0530

    adding sample routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    This is  Hats Page
+</div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
